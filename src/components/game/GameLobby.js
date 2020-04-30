b894295d22ebcf05283f263e33f9fccdd405306e
Oli Kamer <oliver.kamer@uzh.ch>
2020-04-30 17:04:00 +0200

    Proper intendation for GameLobby.js

--- src/components/game/GameLobby.js
+++ src/components/game/GameLobby.js
@@ -38,206 +38,206 @@ import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 
 const styles = theme => ({
-  root: {
-    height: '100vh',
-  },
-  appBar: {
-    position: 'relative',
-    height: 'min-content',
-    backgroundColor: theme.palette.primary.main,
-    color: 'white'
-  },
-  layout: {
-    width: '100%',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-  },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  logoutButton: {
-    width: "100%",
-    marginTop: theme.spacing(2)
-  },
+	root: {
+		height: '100vh',
+	},
+	appBar: {
+		position: 'relative',
+		height: 'min-content',
+		backgroundColor: theme.palette.primary.main,
+		color: 'white'
+	},
+	layout: {
+		width: '100%',
+		marginLeft: theme.spacing(2),
+		marginRight: theme.spacing(2),
+	},
+	paper: {
+		marginTop: theme.spacing(3),
+		marginBottom: theme.spacing(3),
+		padding: theme.spacing(2),
+		[theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+			marginTop: theme.spacing(6),
+			marginBottom: theme.spacing(6),
+			padding: theme.spacing(3),
+		},
+	},
+	stepper: {
+		padding: theme.spacing(3, 0, 5),
+	},
+	buttons: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	logoutButton: {
+		width: "100%",
+		marginTop: theme.spacing(2)
+	},
 });
 
 class GameLobby extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      game: null
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			game: null
+		};
+	}
 
-  logout() {
-    localStorage.removeItem('token');
-    this.props.history.push('/login');
-  }
+	logout() {
+		localStorage.removeItem('token');
+		this.props.history.push('/login');
+	}
 
-  async componentDidMount() {
-    try {
-      /*
-      const response = await api.get('/users');
-      // delays continuous execution of an async operation for 1 second.
-      // This is just a fake async call, so that the spinner can be displayed
-      // feel free to remove it :)
-      await new Promise(resolve => setTimeout(resolve, 1000));
+	async componentDidMount() {
+		try {
+			/*
+			const response = await api.get('/users');
+			// delays continuous execution of an async operation for 1 second.
+			// This is just a fake async call, so that the spinner can be displayed
+			// feel free to remove it :)
+			await new Promise(resolve => setTimeout(resolve, 1000));
 
-      // Get the returned users and update the state.
-      this.setState({ users: response.data });
-       */
-      // Mock the games
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      let game = JSON.parse('{"id":1,"name":"Wau-Game","gameMaster":{"id":2,"name":"Player 2"},"players":[{"id":1,"name":"Player 1"},{"id":2,"name":"Player 2"},{"id":3,"name":"Player 3"},{"id":4,"name":"Player 4"}]}');
-      this.setState({game: game});
+			// Get the returned users and update the state.
+			this.setState({ users: response.data });
+			 */
+			// Mock the games
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			let game = JSON.parse('{"id":1,"name":"Wau-Game","gameMaster":{"id":2,"name":"Player 2"},"players":[{"id":1,"name":"Player 1"},{"id":2,"name":"Player 2"},{"id":3,"name":"Player 3"},{"id":4,"name":"Player 4"}]}');
+			this.setState({game: game});
 
 
-    } catch (error) {
-      alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
-    }
-  }
+		} catch (error) {
+			alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
+		}
+	}
 
-  render() {
-    const { classes } = this.props;
-    const userID = localStorage.getItem('userID');
+	render() {
+		const { classes } = this.props;
+		const userID = localStorage.getItem('userID');
 
-    return (
-        <Grid container component="main" className={classes.root}>
-        <CssBaseline />
-          <AppBar position="absolute" color="default" className={classes.appBar}>
-            <Toolbar>
-              <Grid
-                  justify="space-between"
-                  container
-              >
-                <Grid item>
-                  <Typography variant="h6" color="inherit" noWrap>
-                    {this.state.game?this.state.game.name:`Game`}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Tooltip title="Logout" placement="left">
-                    <Button
-                        color="inherit"
-                        onClick={() => {
-                          this.logout();
-                        }}
-                    >
-                      <ExitToAppIcon />
-                    </Button>
-                  </Tooltip>
-                </Grid>
-              </Grid>
-            </Toolbar>
-          </AppBar>
-          <main className={classes.layout}>
-            <Grid
-                container
-                direction="row"
-                justify="space-evenly"
-                alignItems="flex-start"
-                spacing={4}
-            >
-              <Grid item xs>
-                <Paper className={classes.paper}>
-                  <Typography component="h1" variant="h4" align="center">
-                    {this.state.game?(
-                        <>
-                          Your game
-                        </>
-                      ):`Loading your game`}
-                  </Typography>
-                  {this.state.game ? (
-                      <TableContainer>
-                        <Table size="small">
-                          <TableHead>
-                            <TableRow>
-                              <TableCell>
-                                #
-                              </TableCell>
-                              <TableCell>
-                                Name
-                              </TableCell>
-                              <TableCell>
-                                Leave
-                              </TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {this.state.game.players.map(player => {
-                              return (
-                                  <TableRow key={player.id}>
-                                    <TableCell>
-                                      {player.id}
-                                    </TableCell>
-                                    <TableCell>
-                                      {player.name}
-                                    </TableCell>
-                                    <TableCell>
-                                      {player.id == userID || this.state.game.gameMaster.id == userID?
-                                          (
-                                              <Button>
-                                                <CloseIcon />
-                                              </Button>
-                                          ):(
-                                              <Button disabled>
-                                                <CloseIcon />
-                                              </Button>
-                                          )
-                                      }
-                                    </TableCell>
-                                  </TableRow>
-                              );
-                            })}
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                  ):(
-                      <div>
-                        <p>Loading games</p>
-                        <LinearProgress />
-                      </div>
+		return (
+			<Grid container component="main" className={classes.root}>
+				<CssBaseline />
+				<AppBar position="absolute" color="default" className={classes.appBar}>
+					<Toolbar>
+						<Grid
+							justify="space-between"
+							container
+						>
+							<Grid item>
+								<Typography variant="h6" color="inherit" noWrap>
+									{this.state.game?this.state.game.name:`Game`}
+								</Typography>
+							</Grid>
+							<Grid item>
+								<Tooltip title="Logout" placement="left">
+									<Button
+										color="inherit"
+										onClick={() => {
+											this.logout();
+										}}
+									>
+										<ExitToAppIcon />
+									</Button>
+								</Tooltip>
+							</Grid>
+						</Grid>
+					</Toolbar>
+				</AppBar>
+				<main className={classes.layout}>
+					<Grid
+						container
+						direction="row"
+						justify="space-evenly"
+						alignItems="flex-start"
+						spacing={4}
+					>
+						<Grid item xs>
+							<Paper className={classes.paper}>
+								<Typography component="h1" variant="h4" align="center">
+									{this.state.game?(
+										<>
+											Your game
+										</>
+									):`Loading your game`}
+								</Typography>
+								{this.state.game ? (
+									<TableContainer>
+										<Table size="small">
+											<TableHead>
+												<TableRow>
+													<TableCell>
+														#
+													</TableCell>
+													<TableCell>
+														Name
+													</TableCell>
+													<TableCell>
+														Leave
+													</TableCell>
+												</TableRow>
+											</TableHead>
+											<TableBody>
+												{this.state.game.players.map(player => {
+													return (
+														<TableRow key={player.id}>
+															<TableCell>
+																{player.id}
+															</TableCell>
+															<TableCell>
+																{player.name}
+															</TableCell>
+															<TableCell>
+																{player.id == userID || this.state.game.gameMaster.id == userID?
+																	(
+																		<Button>
+																			<CloseIcon />
+																		</Button>
+																	):(
+																		<Button disabled>
+																			<CloseIcon />
+																		</Button>
+																	)
+																}
+															</TableCell>
+														</TableRow>
+													);
+												})}
+											</TableBody>
+										</Table>
+									</TableContainer>
+								):(
+									<div>
+										<p>Loading games</p>
+										<LinearProgress />
+									</div>
 
-                  ) }
-                </Paper>
-              </Grid>
-              <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                  <Typography component="h1" variant="h4" align="center">
-                    {this.state.game?this.state.game.name:`Loading your game`}
-                  </Typography>
-                  {this.state.game ? (
-                      <>
-                        <strong>Organsier:</strong> {this.state.game.gameMaster.name} <br />
-                        <strong>Currenty Players:</strong> {this.state.game.players.length}
-                      </>
-                  ):(
-                      <div>
-                        <p>Loading game</p>
-                        <LinearProgress />
-                      </div>
-                  ) }
-                </Paper>
-              </Grid>
-            </Grid>
-          </main>
-        </Grid>
-    );
-  }
+								) }
+							</Paper>
+						</Grid>
+						<Grid item xs={4}>
+							<Paper className={classes.paper}>
+								<Typography component="h1" variant="h4" align="center">
+									{this.state.game?this.state.game.name:`Loading your game`}
+								</Typography>
+								{this.state.game ? (
+									<>
+										<strong>Organsier:</strong> {this.state.game.gameMaster.name} <br />
+										<strong>Currenty Players:</strong> {this.state.game.players.length}
+									</>
+								):(
+									<div>
+										<p>Loading game</p>
+										<LinearProgress />
+									</div>
+								) }
+							</Paper>
+						</Grid>
+					</Grid>
+				</main>
+			</Grid>
+		);
+	}
 }
 
 export default withRouter(withStyles(styles)(GameLobby));
