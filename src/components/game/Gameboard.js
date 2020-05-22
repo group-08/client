f5baae8273836358ed4fe9023f31bb9baa989779
Oli Kamer <oliver.kamer@uzh.ch>
2020-05-22 15:49:12 +0200

    Display number of cards distributed

--- src/components/game/Gameboard.js
+++ src/components/game/Gameboard.js
@@ -358,7 +358,7 @@ class Gameboard extends React.Component {
     }
 
     getName(playerId) {
-	    if ( this.state.game && this.state.game.players ) {
+	    if (this.state.game && this.state.game.players ) {
 		    let player = this.playerFromPlayerId(playerId);
 		    console.log(playerId, player);
 		    // For humans
@@ -525,6 +525,10 @@ class Gameboard extends React.Component {
 		} else {
     		this.move();
 		}
+	}
+
+	numberofCards(gameCardNum) {
+    	return (gameCardNum -1)%5 + 2; // Formula by very smart trial and error
 	}
 
 	isFinished() {
@@ -959,14 +963,44 @@ class Gameboard extends React.Component {
 					        {this.state.game && this.state.game.weatherState && this.state.game.city?
 						        (
 							        <Paper className={classes.weatherBox}>
-								        <ReactAnimatedWeather
-									        icon={weathers[this.state.game.weatherState]}
-								        />
+								        <Grid
+									        container
+									        justify="space-evenly"
+									        alignItems="center"
+								        >
+									        <Grid item xs>
+										        <ReactAnimatedWeather
+											        icon={weathers[this.state.game.weatherState]}
+										        />
+									        </Grid>
+									        {this.state.game.cardNum?
+										        <Grid item xs>
+											        {this.numberofCards(this.state.game.cardNum)}
+										        </Grid>
+									        :''}
+
+								        </Grid>
+
 								        <Divider />
-								        <Typography variant="body1">
-											You now travelled to <Typography variant="h6" >{cities[this.state.game.city]}</Typography>
-											where the weather is currently {this.state.game.weatherState.toLowerCase()}.
-								        </Typography>
+								        <Grid
+									        container
+									        justify="space-evenly"
+									        alignItems="center"
+								        >
+									        <Grid item xs>
+										        <Typography variant="body1">
+											        You now travelled to <strong>{cities[this.state.game.city]}</strong> where the weather is currently {this.state.game.weatherState.toLowerCase()}.
+										        </Typography>
+									        </Grid>
+									        {this.state.game.cardNum?
+										        <Grid item xs>
+											        <Typography variant="body1">
+												        Everyone received {this.numberofCards(this.state.game.cardNum)} cards.
+											        </Typography>
+										        </Grid>
+									        :''}
+								        </Grid>
+
 							        </Paper>
 						        ):''}
 				        </Grid>
